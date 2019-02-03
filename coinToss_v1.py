ee8af2908ddd4b948f9d082b560a9388057421bc
Pijus <44477147+PJurc@users.noreply.github.com>
2019-02-03 20:31:48 +0200

    Updated coinToss_v1

--- coinToss_v1.py
+++ coinToss_v1.py
@@ -1,19 +1,23 @@
 import random
 
-def coin(): #function that generates either 0 (heads) or 1 (tails)
+def coin():
+    """
+    A function that randomly generates either 0 or 1
+    using the 'random' library and the 'randint' variable
+    of the library.
+    The result is then returned to the main program.
+    """
     r = random.randint(0, 1)
-    return r #returns the generated result
+    return r
 
 choice = input("Enter the choice of the coin side (heads/tails): ")
-#print(choice) test if choice is returned correctly
-if(choice != "heads" and choice != "tails"): #if input is not as asked
+if(choice != "heads" and choice != "tails"): #if input is not as asked, return an error and quit the program.
     print("ERROR. Incorrect output, please enter 'heads' or 'tails' only.")
     quit()
 
 r = coin() #assign r as coin() returned variable
-#print(r) test if r is returned correctly
 
-if((r == 0 and choice == "heads") or (r == 1 and choice == "tails")): #If statement to decide whether the flip counts as a win or loss
-    print("You flipped", choice, "and won. Congratulations!")
+if((r == 0 and choice == "heads") or (r == 1 and choice == "tails")): #If statement to decide whether the flip counts
+        print("You flipped", choice, "and won. Congratulations!")       #as a win or loss
 else:
         print("You flipped", choice, "and lost. Better luck next time!")
